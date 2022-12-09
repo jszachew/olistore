747687a668b29c3997b84cfede79f54d41c68b52
Jakub Szachewicz <jszachewicz@MacBook-Air-Jakub.local>
2022-12-09 20:15:07 +0100

    ga added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import './App.css';
 
 function App() {
   return (
+    <div className="box">
+
     <div className='main'>
       <iframe title="konon" width="100%" min-height="100%"
         src="https://www.youtube.com/embed/DaMYTIE-6Jw?controls=0&autoplay=1&mute=1&loop=1">
       </iframe>
     </div>
+    <div className="greetings">
+      <span>!!NIEBAWEM NA SPRZEDAZ!!</span>
+
+    </div>
+    </div>
+
   );
 }
 
