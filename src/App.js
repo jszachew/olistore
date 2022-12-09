0f692293c2c94a85d2368f7ceaa025d885092a9f
Jakub Szachewicz <jszachewicz@MacBook-Air-Jakub.local>
2022-12-09 19:56:28 +0100

    hehe

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className='main'>
-      <iframe width="100%" min-height="100%"
+      <iframe title="konon" width="100%" min-height="100%"
         src="https://www.youtube.com/embed/DaMYTIE-6Jw?controls=0&autoplay=1&mute=1&loop=1">
       </iframe>
     </div>
