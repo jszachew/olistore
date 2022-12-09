84db7a47e92f6154d8ea07a68a42ad840ab0b161
Jakub Szachewicz <jszachewicz@MacBook-Air-Jakub.local>
2022-12-09 21:17:43 +0100

    ga added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ function App() {
   return (
     <div className="box">
 <div className="greetings">
-      <span>!!NIEBAWEM NA SPRZEDAZ!!</span>
+      <span>NIEBAWEM NA SPRZEDAZ</span>
 
     </div>
     <div className='main'>
     
-      <iframe title="konon" width="100%" min-height="100%"
-        src="https://www.youtube.com/embed/DaMYTIE-6Jw?controls=0&autoplay=1&mute=1&loop=1">
-      </iframe>
+      <img src="konon-cycochy.gif">
+      </img>
     </div>
 
     </div>
